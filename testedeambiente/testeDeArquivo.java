37b0138e6268dd5817d4fae8bf062d9affdfe793
adrianops24 <adrianpsilva90@gmail.com>
2025-03-09 21:19:54 -0300

    primeiros testes com manipulação de variaveis e exibição no console

---
+++ testedeambiente/testeDeArquivo.java
@@ -0,0 +1,17 @@
+package cursjava.testedeambiente;
+public class testeDeArquivo {
+public static void main(String [] args){
+
+int num =100;
+int n1=20; 
+int n2=30;
+int n3=40;
+
+int soma = n1 + n2 + n3;
+
+ System.out.println("alo mudo");
+ System.out.printf("%s  ano: %d%n", "O sixers vai ser campeao\n", 2026);
+System.out.printf( "valor da váriavel: %d%nResto: %d ", num, soma   );
+}
+    
+}
